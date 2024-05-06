a4dc2d06a7973da55cba58a6a0aaab3c7bfdb0f5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-06 10:27:51 +0300

    Implementing NavBar

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,30 @@
-import React from 'react'
+'use client'
 
-const NavItem = () => {
+import { PRODUCT_CATEGORIES } from '@/config'
+import { Button } from './ui/button'
+import { ChevronDown } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+
+type Category = (typeof PRODUCT_CATEGORIES)[number]
+
+interface Props {
+    category: Category
+    handleOpen: () => void
+    close: () => void
+    isOpen: boolean
+    isAnyOpen: boolean
+  }
+
+const NavItem:FC<Props> = ({
+    isAnyOpen,
+    category,
+    handleOpen,
+    close,
+    isOpen,
+}) => {
   return (
     <div>NavItem</div>
   )
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+import NavItem from './NavItem'
 
 const NavItems = () => {
-  return (
-    <div>NavItems</div>
-  )
+    const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+            setActiveIndex(null)
+            }
+        }
+
+        document.addEventListener('keydown', handler)
+
+        return () => {
+            document.removeEventListener('keydown', handler)
+        }
+    }, []);
+    
+    const isAnyOpen = activeIndex !== null;
+    const navRef = useRef<HTMLDivElement | null>(null);
+    return (
+        <div className='flex gap-4 h-full'>
+
+        </div>
+    )
 }
 
 export default NavItems
