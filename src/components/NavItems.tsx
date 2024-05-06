9297b66828ea4ad4f20ff1c75185e1a373400d16
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-06 11:16:45 +0300

    Created hooks

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef, useState } from 'react'
 import NavItem from './NavItem'
 import { PRODUCT_CATEGORIES } from '@/config';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
@@ -23,6 +24,10 @@ const NavItems = () => {
     
     const isAnyOpen = activeIndex !== null;
     const navRef = useRef<HTMLDivElement | null>(null);
+
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+
+    
     return (
         <div className='flex gap-4 h-full'>
             {PRODUCT_CATEGORIES.map((category,i)=>{
