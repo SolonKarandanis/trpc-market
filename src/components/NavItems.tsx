c90981f68e50866f5b9906a60ade49fa996ea808
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-06 11:11:31 +0300

    Created Navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react'
 import NavItem from './NavItem'
+import { PRODUCT_CATEGORIES } from '@/config';
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
@@ -24,7 +25,30 @@ const NavItems = () => {
     const navRef = useRef<HTMLDivElement | null>(null);
     return (
         <div className='flex gap-4 h-full'>
+            {PRODUCT_CATEGORIES.map((category,i)=>{
+                const handleOpen = () => {
+                    if (activeIndex === i) {
+                      setActiveIndex(null)
+                    } else {
+                      setActiveIndex(i)
+                    }
+                  }
+          
+                  const close = () => setActiveIndex(null)
+          
+                  const isOpen = i === activeIndex
 
+                return (
+                    <NavItem
+                        category={category}
+                        close={close}
+                        handleOpen={handleOpen}
+                        isOpen={isOpen}
+                        key={category.value}
+                        isAnyOpen={isAnyOpen}
+                    />
+                )
+            })}
         </div>
     )
 }
