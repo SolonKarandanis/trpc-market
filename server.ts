da57182068c342df035c842cf866569e781d42e6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-07 10:48:04 +0300

    Setting up payload cms

--- server.ts
+++ server.ts
@@ -1,1 +1,43 @@
+import { getPayloadClient } from "@/get-payload";
+import { nextApp, nextHandler } from "@/next-utils";
 import express from "express";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000
+
+const start = async () =>{
+    const payload =  await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+              cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+            },
+        },
+    });
+
+    app.use((req, res) => nextHandler(req, res));
+
+    nextApp.prepare().then(()=>{
+        payload.logger.info('Next.js started');
+
+        app.listen(PORT, async () => {
+            payload.logger.info(
+              `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+            )
+        });
+    });
+
+    // if (process.env.NEXT_BUILD){
+    //     app.listen(PORT, async () => {
+    //         payload.logger.info(
+    //           'Next.js is building for production'
+    //         )
+    //         // @ts-expect-error
+    //         await nextBuild(path.join(__dirname, '../'))
+    //         process.exit()
+    //     })
+    //     return
+    // }
+}
+
+start();
