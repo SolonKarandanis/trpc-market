05a5e3a1a9a3eeeebba446f54ee24aa229e29f0f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-07 09:24:03 +0300

    Implementing cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,13 +1,18 @@
 "use client"
 
 import { ShoppingCart } from 'lucide-react'
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
 import { Separator } from './ui/separator';
+import { formatPrice } from '@/lib/utils';
+import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import Image from 'next/image';
 
 
 
 const Cart = () => {
-    const itemCount = 1;
+    const itemCount = 0;
+    const fee=1;
 
     return (
         <Sheet>
@@ -39,12 +44,50 @@ const Cart = () => {
                                 </div>
                                 <div className='flex'>
                                     <span className='flex-1'>Transaction Fee</span>
-                                    <span>1</span>
+                                    <span>{formatPrice(fee)}</span>
+                                </div>
+                                <div className='flex'>
+                                    <span className='flex-1'>Total</span>
+                                    <span>{formatPrice(fee)}</span>
                                 </div>
                             </div>
+                            <SheetFooter>
+                                <SheetTrigger asChild>
+                                    <Link href='/cart' 
+                                        className={buttonVariants({
+                                            className:"w-full"
+                                        })}>
+                                        Continue to Checkout
+                                    </Link>
+                                </SheetTrigger>
+                            </SheetFooter>
                         </div>
                     </>
-                ):(<div></div>)}
+                ):(
+                <div className='flex h-full flex-col items-center 
+                    justify-center space-y-1'>
+                        <div aria-hidden='true' 
+                            className='relative mb-4 h-60 w-60 text-muted-foreground'>
+                            <Image 
+                                src='/hippo-empty-cart.png' 
+                                fill
+                                alt='empty cart'/>
+                        </div>
+                        <div className='text-xl font-semibold'>
+                            Your cart is empty
+                        </div>
+                        <SheetTrigger asChild>
+                            <Link href='/products'
+                                className={buttonVariants({
+                                    variant: "link",
+                                    size:"sm",
+                                    className:"text-sm text-muted-foreground"
+                                })}>
+                                Add Items to your Cart
+                            </Link>
+                        </SheetTrigger>
+                </div>
+                )}
             </SheetContent>
         </Sheet>
     )
