e29d0420381bfc7551e4491faed6f8f5c11e620b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-14 09:38:21 +0300

    Setting up trpc

---
+++ src/trpc/trpc.ts
@@ -0,0 +1,8 @@
+// import { ExpressContext } from '@/server'
+import { TRPCError, initTRPC } from '@trpc/server'
+
+const t = initTRPC.context().create()
+const middleware = t.middleware
+
+export const router = t.router
+export const publicProcedure = t.procedure
