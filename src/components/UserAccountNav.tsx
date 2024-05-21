a279242a626427f587e2cc9a5ce37726ac7855bd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-21 09:26:01 +0300

    Implemented UserAccountNav

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { FC } from 'react'
-
-
 import { User } from '@/payload-types'
 import { Button } from './ui/button'
 import {
@@ -13,14 +11,50 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 import Link from 'next/link'
+import { useAuth } from '@/hooks/use-auth'
 
 interface Props{
     user:User
 }
 
-const UserAccountNav:FC<Props> = () => {
+const UserAccountNav:FC<Props> = ({user}) => {
+  const { signOut } = useAuth()
   return (
-    <div>UserAccountNav</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        className='overflow-visible'>
+        <Button
+          variant='ghost'
+          size='sm'
+          className='relative'>
+          My account
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='bg-white w-60'
+        align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-0.5 leading-none'>
+            <p className='font-medium text-sm text-black'>
+              {user.email}
+            </p>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        
+        <DropdownMenuItem asChild>
+          <Link href='/sell'>Seller Dashboard</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem
+          onClick={signOut}
+          className='cursor-pointer'>
+          Log out
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
