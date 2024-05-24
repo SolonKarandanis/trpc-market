ca22fd4b5e816a49b4247fb3d354e81360d2c10b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-24 08:52:55 +0300

    Implementing ProductListing

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -16,9 +16,55 @@ const ProductListing:FC<ProductListingProps> = ({
     product,
     index,
 }) => {
-  return (
-    <div>ProductListing</div>
-  )
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setIsVisible(true)
+        }, index * 75)
+
+        return () => clearTimeout(timer)
+    }, [index])
+
+    if (!product || !isVisible) return <ProductPlaceholder />
+
+    const label = PRODUCT_CATEGORIES.find(
+        ({ value }) => value === product.category
+    )?.label
+    
+    const validUrls = product.images
+        .map(({ image }) =>
+            typeof image === 'string' ? image : image.url
+        )
+        .filter(Boolean) as string[]
+
+    if (isVisible && product){
+        return (
+            <Link
+                className={cn(
+                'invisible h-full w-full cursor-pointer group/main',
+                {
+                    'visible animate-in fade-in-5': isVisible,
+                }
+                )}
+                href={`/product/${product.id}`}>
+
+            </Link> 
+        )
+    }
 }
+
+const ProductPlaceholder = () => {
+    return (
+      <div className='flex flex-col w-full'>
+        <div className='relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl'>
+          {/* <Skeleton className='h-full w-full' /> */}
+        </div>
+        {/* <Skeleton className='mt-4 w-2/3 h-4 rounded-lg' />
+        <Skeleton className='mt-2 w-16 h-4 rounded-lg' />
+        <Skeleton className='mt-2 w-12 h-4 rounded-lg' /> */}
+      </div>
+    )
+  }
 
 export default ProductListing
