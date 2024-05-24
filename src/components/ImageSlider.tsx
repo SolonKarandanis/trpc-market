647c404be331de459a54c425811475a711f350cc
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-24 08:56:21 +0300

    Creating ImageSlider

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+
+interface ImageSliderProps {
+    urls: string[]
+}
+
+const ImageSlider:FC<ImageSliderProps> = ({urls}) => {
+  return (
+    <div>ImageSlider</div>
+  )
+}
+
+export default ImageSlider
--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import {FC,useEffect, useState} from 'react'
-// import { Skeleton } from './ui/skeleton'
+import { Skeleton } from './ui/skeleton'
 import Link from 'next/link'
 import { cn, formatPrice } from '@/lib/utils'
 import { PRODUCT_CATEGORIES } from '@/config'
 import { Product } from '@/payload-types'
+import ImageSlider from './ImageSlider'
 
 interface ProductListingProps {
     product: Product | null
@@ -48,7 +49,18 @@ const ProductListing:FC<ProductListingProps> = ({
                 }
                 )}
                 href={`/product/${product.id}`}>
-
+                <div className='flex flex-col w-full'>
+                    <ImageSlider urls={validUrls} />
+                    <h3 className='mt-4 font-medium text-sm text-gray-700'>
+                        {product.name}
+                    </h3>
+                    <p className='mt-1 text-sm text-gray-500'>
+                        {label}
+                    </p>
+                    <p className='mt-1 font-medium text-sm text-gray-900'>
+                        {formatPrice(product.price)}
+                    </p>
+                </div>  
             </Link> 
         )
     }
@@ -58,11 +70,11 @@ const ProductPlaceholder = () => {
     return (
       <div className='flex flex-col w-full'>
         <div className='relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl'>
-          {/* <Skeleton className='h-full w-full' /> */}
+          <Skeleton className='h-full w-full' />
         </div>
-        {/* <Skeleton className='mt-4 w-2/3 h-4 rounded-lg' />
+        <Skeleton className='mt-4 w-2/3 h-4 rounded-lg' />
         <Skeleton className='mt-2 w-16 h-4 rounded-lg' />
-        <Skeleton className='mt-2 w-12 h-4 rounded-lg' /> */}
+        <Skeleton className='mt-2 w-12 h-4 rounded-lg' />
       </div>
     )
   }
