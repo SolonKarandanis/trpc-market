a41c3f183d3170154c1c6a0edc7334915ff134c5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-29 09:10:48 +0300

    Setting up stripe webhook

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import express from "express";
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { inferAsyncReturnType } from '@trpc/server';
 import { appRouter } from './trpc'
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
+import { stripeWebhookHandler } from './webhooks';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000
@@ -20,35 +23,51 @@ export type ExpressContext = inferAsyncReturnType<
   typeof createContext
 >
 
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
+
 const start = async () =>{
-    const payload =  await getPayloadClient({
-        initOptions: {
-            express: app,
-            onInit: async (cms) => {
-              cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
-            },
-        },
-    });
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
+  
+  app.post(
+    '/api/webhooks/stripe',
+    webhookMiddleware,
+    stripeWebhookHandler
+  )
 
-    app.use(
-        '/api/trpc',
-        trpcExpress.createExpressMiddleware({
-          router: appRouter,
-          createContext,
-        })
-      )
+  const payload =  await getPayloadClient({
+      initOptions: {
+          express: app,
+          onInit: async (cms) => {
+            cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+          },
+      },
+  });
 
-    app.use((req, res) => nextHandler(req, res));
+  app.use(
+      '/api/trpc',
+      trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+      })
+    )
 
-    nextApp.prepare().then(()=>{
-        payload.logger.info('Next.js started');
+  app.use((req, res) => nextHandler(req, res));
 
-        app.listen(PORT, async () => {
-            payload.logger.info(
-              `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-            )
-        });
-    });
+  nextApp.prepare().then(()=>{
+      payload.logger.info('Next.js started');
+
+      app.listen(PORT, async () => {
+          payload.logger.info(
+            `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+          )
+      });
+  });
 
     // if (process.env.NEXT_BUILD){
     //     app.listen(PORT, async () => {
