7ceeadefd5213cb4705ef274fa73575cd402200d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-29 08:52:12 +0300

    Implemented PaymentStatus

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -15,9 +15,41 @@ const PaymentStatus:FC<PaymentStatusProps> = ({
     orderId,
     isPaid,
 }) => {
-  return (
-    <div>PaymentStatus</div>
-  )
+    const router = useRouter()
+
+    const { data } = trpc.payment.pollOrderStatus.useQuery(
+        { orderId },
+        {
+        enabled: isPaid === false,
+        refetchInterval: (data) =>
+            data?.isPaid ? false : 1000,
+        }
+    )
+
+    useEffect(() => {
+        if (data?.isPaid) router.refresh()
+    }, [data?.isPaid, router])
+
+    return (
+        <div className='mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600'>
+            <div>
+                <p className='font-medium text-gray-900'>
+                Shipping To
+                </p>
+                <p>{orderEmail}</p>
+            </div>
+            <div>
+                <p className='font-medium text-gray-900'>
+                Order Status
+                </p>
+                <p>
+                {isPaid
+                    ? 'Payment successful'
+                    : 'Pending payment'}
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default PaymentStatus
